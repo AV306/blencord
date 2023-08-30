004b9d6e393c3ef2de8023a26048793d4021d599
AV306 <66512356+AV306@users.noreply.github.com>
2023-08-30 10:30:58 +0000

    AaAAAAAAAaaaAAA

--- src/main/java/me/av306/blencord/Main.java
+++ src/main/java/me/av306/blencord/Main.java
@@ -28,11 +28,24 @@ public class Main
 
 		// Set up discord api access
 
-		System.out.println( "Beginning communication with Discord API..." );
+		System.out.println( "Connecting to Discord..." );
 
 		DiscordApi api = new DiscordApiBuilder()
 				.setToken( token ) // Set token
 				.login() // Login to API
 				.join(); // Block until login completed
+		
+		System.out.println( "Connected to Discord!" );
+
+		// Main event loop
+		String line;
+		while ( (line = scanner.nextLine()) != ":q" )
+		{
+				
+		}
+
+		// Quit
+		System.out.println( "Quitting Blencord! Goodbye!" );
+		System.exit( 0 );
 	}
 }
